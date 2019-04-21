e31ef047fe578ad3db8484515d1bc3a6aadc078f
zzznever <zzz.never@gmail.com>
2019-04-21 00:44:05 -0700

    refactor mtg card search

--- main2.py
+++ main2.py
@@ -11,6 +11,7 @@ async def on_ready():
     game = discord.Game("01101111011101100110111001101001")
     await client.change_presence(status=discord.Status.online, activity=game)
 
+# TODO  Respond to any message with they key word "hal" as the first or last word
 @client.event
 async def on_message(message):
     if message.author == client.user:
@@ -30,21 +31,22 @@ async def on_message(message):
 
     if message.content.startswith('$card'):
         card_name = message.content[6:]
-        card = Card.where(name=card_name).all()
-        if not card:
-            await message.channel.send('Error: cannot find card {0.content}'.format(client))
-        else:
-            if card[0].image_url is None:
-                await message.channel.send(card[1].image_url)
-            else:
-                await message.channel.send(card[0].image_url)
+        try:
+            # Add all found cards to a list.
+            card_list = Card.where(name=card_name).array()
 
-        #fmt = '{0.author.name} has deleted the message:\n{0.content}'
-        #await message.author.send(fmt.format(message))
+            # Get the last row in the array, which (theoretically) is the most recent printing.
+            card = card_list[card_list.__len__() - 1]
 
+            # Send picture of card.
+            await message.channel.send(card['imageUrl'])
 
+            # TODO  Send URL of page instead of image
+        except IndexError:
+            await message.channel.send("Unable to find card \"{0}\".".format(card_name))
 
+    #  Execute script on host
    # if message.content.startswith('$doit'):
    #     pid = subprocess.Popen([sys.executable, r'C:\Users\cm\PycharmProjects\untitled\main.py'])
 
-client.run('')
+client.run('NTI0OTEzOTE1NDYxODk0MTU1.XLvNAA.U0_UAuKMM5_Wq3lVRiROkFUpbAM')
